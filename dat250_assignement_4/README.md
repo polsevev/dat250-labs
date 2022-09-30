8b1c27899228ff857f3593dbd6d48f14c0648237
polsevev <rolf.martin@glomsrud.no>
2022-09-30 13:28:35 +0200

    Finished assignement 4

---
+++ dat250_assignement_4/README.md
@@ -0,0 +1,5 @@
+# Challenges:
+
+Perhaps instead of having us dig through the tests, make a sort of map of the endpoints and how they are supposed to be interracted with. As digging through tests are not a fast way of discovering the design of the endpoint. 
+
+Else i had no issues with this assignement
---
+++ dat250_assignement_4/todos/src/main/java/no/hvl/dat250/rest/todos/TodoAPI.java
@@ -0,0 +1,103 @@
+package no.hvl.dat250.rest.todos;
+
+import com.google.gson.Gson;
+import okhttp3.MediaType;
+
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static spark.Spark.*;
+
+/**
+ * Rest-Endpoint.
+ */
+public class TodoAPI {
+
+    static ArrayList<Todo> todos = new ArrayList<>();
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            port(Integer.parseInt(args[0]));
+        } else {
+            port(9000);
+        }
+        Gson gson = new Gson();
+
+        //I assume doing this is okay as the assignement does not state we need to use JPA so i just make the Ids myself
+        AtomicLong idCount = new AtomicLong();
+        after((req, res) -> {
+            res.type("application/json");
+        });
+        post("/todos", ((request, response) -> {
+
+            Todo sent = gson.fromJson(request.body(), Todo.class);
+            if (sent.getId() == null){
+                sent = new Todo(idCount.get(), sent.getSummary(), sent.getDescription());
+                idCount.addAndGet(1);
+            }
+            todos.add(sent);
+
+            return gson.toJson(sent);
+        }));
+
+        get("/todos", ((request, response) -> {
+            return gson.toJson(todos, ArrayList.class);
+        }));
+
+        get("/todos/:id", ((request, response) -> {
+            String idString = request.params(":id");
+            Long id = null;
+            try{
+                id = Long.parseLong(idString,10);
+            }catch (Exception e){
+                return String.format("The id \"%s\" is not a number!", idString);
+            }
+            for (Todo todo: todos) {
+                if (Objects.equals(todo.getId(), id)){
+                    return gson.toJson(todo);
+                }
+            }
+            return String.format("Todo with the id \"%s\" not found!", id);
+        }));
+
+        delete("/todos/:id", ((request, response) -> {
+            String idString = request.params(":id");
+            Long id = null;
+            try{
+                id = Long.parseLong(idString,10);
+            }catch (Exception e){
+                return String.format("The id \"%s\" is not a number!", idString);
+            }
+            for (int i = 0; i < todos.size(); i++){
+                if(Objects.equals(todos.get(i).getId(), id)){
+                    todos.remove(i);
+                    return "Deletion successfull!";
+                }
+            }
+            return String.format("Todo with the id \"%s\" not found!", idString);
+        }));
+
+
+        put("/todos/:id", ((request, response) -> {
+            String idString = request.params(":id");
+            Long id = null;
+            try{
+                id = Long.parseLong(idString,10);
+            }catch (Exception e){
+                return String.format("The id \"%s\" is not a number!", idString);
+            }
+            Todo sent = gson.fromJson(request.body(), Todo.class);
+            for (int i = 0; i < todos.size(); i++) {
+                if(Objects.equals(todos.get(i).getId(), id)){
+                    todos.set(i, sent);
+                    break;
+                }
+            }
+            return "Successfull update!";
+
+        }));
+
+    }
+}
